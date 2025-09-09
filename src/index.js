f0823c1f0aaeebc6b92f3363715ed915641529c2
zametki.frontendera <zametki.frontendera@gmail.com>
2025-09-09 16:21:40 +0300

    fix bug for register cors

--- src/index.js
+++ src/index.js
@@ -2,19 +2,20 @@ import express from 'express';
 import cors from 'cors';
 import { config } from './config/env.js';
 import { routes } from './routes.js';
+import authRouter from "./routes/auth.js";
 
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: config.corsOrigins,
-    credentials: true,
-  })
-);
 
-app.get('/health', (_req, res) => res.json({ ok: true, ts: Date.now() }));
+app.use(cors({
+  origin: "http://vmetke.ru",
+  credentials: true,
+}));
+
 app.use('/api', routes);
+
+app.use("/auth", authRouter);
 
 app.listen(config.port, () => {
   console.log(`API listening on http://localhost:${config.port}`);
