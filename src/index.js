51cfd9fb4800cda8d6b76a5c447c4dbc5e580a80
zametki.frontendera <zametki.frontendera@gmail.com>
2025-09-09 16:26:36 +0300

    fix bug for register cors

--- src/index.js
+++ src/index.js
@@ -10,12 +10,19 @@ app.use(express.json());
 
 app.use(cors({
   origin: "http://vmetke.ru",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
 }));
 
 app.use('/api', routes);
 
 app.use("/auth", authRouter);
+
+app.options("*", cors({
+  origin: "http://vmetke.ru",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+}));
 
 app.listen(config.port, () => {
   console.log(`API listening on http://localhost:${config.port}`);
